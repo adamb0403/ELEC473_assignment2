a59222ae5e7980a65b0265770f4b3768250aa539
Butt <sgabutt@liverpool.ac.uk>
2024-02-07 16:52:18 +0000

    UART working on board

--- tx_baud_counter/tx_baud_counter.v
+++ tx_baud_counter/tx_baud_counter.v
@@ -7,14 +7,14 @@ module tx_baud_counter (input clk,
     reg [10:0] count; // 11 bit register to count upto the baud rate
 	
     // Output 1 when the baud target has been reached
-	assign baud_output = (count == 11'd3); // 1301 clk cycles at 20ns = 38400 bps baud rate
-    assign half_baud_output = (count == 11'd1);
+	assign baud_output = (count == 11'd1301); // 1301 clk cycles at 20ns = 38400 bps baud rate
+    assign half_baud_output = (count == 11'd650);
     always @(posedge clk) begin
         if (!reset || reset_count) begin
             count <= 0; // Reset the count upon reset
         end
         else begin
-            if (count == 3) begin
+            if (count == 1301) begin
                 count <= 0; // Reset the count if the baud target has been reached
             end
             else begin
